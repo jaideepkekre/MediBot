5dc9a9852542cf412c2cabc41f38502231f23ae3
Jaideep Kekre <jaideepkekre@gmail.com>
2016-01-04 12:02:41 +0530

    Create core.py
    added doc info

---
+++ core.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#DATES : 4JAN15 , ___?___ 
+#CORE : called by CONSUMER.
+#USE : To read JSON object , and perform functions on it . 
+#FUNCTIONS: getJSON() , readJSON() , tokenizeResponse() , languageMagic() 
+#Owner : Jaideep Kekre 
+#Issues: "Sameer put your issues here"
