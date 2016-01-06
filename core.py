c6ddad65835c3b5890b17f995de23ec43fe11b29
jaideepkekre <jaideepkekre@gmail.com>
2016-01-06 16:13:02 +0530

    added scaffloding code in core()

--- core.py
+++ core.py
@@ -1,6 +1,61 @@
 #DATES : 4JAN15 , ___?___ 
-#CORE : called by CONSUMER.
+#CORE : called by dispatcher.py.
 #USE : To read JSON object , and perform functions on it . 
-#FUNCTIONS: getJSON() , readJSON() , tokenizeResponse() , languageMagic() 
+#FUNCTIONS: getStruct() , readStruct() , tokenizeResponse() , languageMagic() 
 #Owner : Jaideep Kekre 
 #Issues: "Sameer put your issues here"
+
+
+class core(object):
+  """one stop shop for all your NLP needs"""
+  def __init__(self,argUserID):
+    super (core, self).__init__()
+    self.id = argUserID
+    print(self.id)
+
+  def getStruct(self,argDict):
+    #perform checks on Dict
+    #entryPoint
+    print("Hi from getStruct")
+    pass
+
+  def readStruct(self):
+    #parse Dict and extract relevant info
+    #calls tokenizeResponse()
+    print("Hi from readStruct")
+    pass
+
+  def tokenizeResponse(self,argString):
+    #tokenizes a string and returns a list of tokens(optional)
+    print("Hi from tokenizeResponse")
+    pass
+
+  def languageMagic(self,argListOFtokens):
+    #gets a list of tokens as arg and does magic on them
+    #calls response()
+    print("Hi from languageMagic")
+    pass
+  def response(self,argList):
+    #passes contents of argList to console / dispatcher
+    #exitPoint
+    print("Hi from response")
+    pass
+
+def main():
+  print("This is a class , don't run this directly")
+  sampleOBJ=core("Kekre")
+  sampleOBJ.getStruct("sample")
+  sampleOBJ.readStruct()
+  sampleOBJ.tokenizeResponse("SampleString")
+  sampleOBJ.languageMagic(['a','b','c'])
+
+if __name__ == '__main__':
+  main()
+
+
+
+    
+  
+
+
+
--- dispatcher.py
+++ dispatcher.py
@@ -1,4 +1,9 @@
 #!usr/bin/python
-#Owner :Jaideep Kekre
-#_author_ = Jaideep Kekre
-#_info_   = This module contains a Python Script 
+#DATES : 4JAN15 , ___?___ 
+#DISPATCHER : called by server.py.
+#USE : To read DICT object , and assign it to correct core object . 
+#FUNCTIONS:  
+#Owner : Jaideep Kekre 
+#Issues: "Sameer put your issues here"
+
+import core
