387c3df2cfa5d2c2a1e3236b535f6586caed3143
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2016-01-07 15:39:31 +0530

    Implemented a multi threaded non blocking server for processing messages
    Implemented a multi threaded non blocking server for processing messages
    
    delete shiz

--- dispatcher.py
+++ dispatcher.py
@@ -8,6 +8,27 @@
 
 import core
 
+# @author Sameer Deshmukh
+class dispatcher():
+  """
+  class called by a thread that gets a dict and talks to core for 
+  further processing.
+  """
+  def __init__(self, message_params):
+    self.message_params = message_params
+    
+  def is_message_ready(self):
+    return True # change this with a method from core that tells if the message is ready
+
+  def response(self):
+    # TODO: Change this 
+    d = {
+      'chat_id' : self.message_params['chat_id'],
+      'text' : "THIS IS WHAT YOU SEND ME BRUH :: " + self.message_params['text']
+    }
+
+    return d
+
 def main():
 	coreobj=core.core("kekre")
 	coreobj.run_core("")
--- server.py
+++ server.py
@@ -1,10 +1,33 @@
+#DATES : 4 jan 2016
+#USE : Poll telegram API for incoming messages and forward them to core
+# after wrapping in a nice JSON format.
+#Owner : @v0dro
+
 from telegram import Updater
+from core import core
+import dispatcher
+import threading
 import os
 
-def start(bot, update):
-  bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+def message_dispatcher(bot, update):
+  d = {
+    'chat_id' : update.message.chat_id,
+    'text' : update.message.text
+  }
+  creator = dispatcher.dispatcher(d)
+  while not creator.is_message_ready():
+    pass
+
+  m = creator.response()
+  bot.sendMessage(chat_id=m['chat_id'], text=m['text'])
+
+def process_messages(bot, update):
+  t = threading.Thread(target=message_dispatcher, args=(bot, update))
+  t.daemon = True
+  t.start()
 
 updater = Updater(token=os.environ.get('TELEGRAM_API_KEY'))
-dispatcher = updater.dispatcher
-dispatcher.addTelegramCommandHandler('start', start)
+message_sender = updater.dispatcher
+message_sender.addTelegramMessageHandler(process_messages)
 updater.start_polling()
+ 
