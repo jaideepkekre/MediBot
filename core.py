a1c2b3aafde79a802a65519e35125b65a3e83bae
jaideepkekre <jaideepkekre@gmail.com>
2016-01-07 14:58:48 +0530

    killed the camel , buds with a snake . Added a run_core()

--- core.py
+++ core.py
@@ -13,41 +13,58 @@ class core(object):
     self.id = argUserID
     print(self.id)
 
-  def getStruct(self,argDict):
+#########################################
+
+  def get_struct(self,argDict):
     #perform checks on Dict
     #entryPoint
     print("Hi from getStruct")
     pass
 
-  def readStruct(self):
+
+
+  def read_struct(self):
     #parse Dict and extract relevant info
     #calls tokenizeResponse()
     print("Hi from readStruct")
     pass
 
-  def tokenizeResponse(self,argString):
+
+  def tokenize_response(self,argString):
     #tokenizes a string and returns a list of tokens(optional)
     print("Hi from tokenizeResponse")
     pass
 
-  def languageMagic(self,argListOFtokens):
+
+  def language_magic(self,argListOFtokens):
     #gets a list of tokens as arg and does magic on them
     #calls response()
     print("Hi from languageMagic")
     pass
+
+
   def response(self,argList):
     #passes contents of argList to console / dispatcher
     #exitPoint
     print("Hi from response")
     pass
 
+  def run_core(self,argDict):
+    self.get_struct("sample")
+    self.read_struct()
+    self.tokenize_response("SampleString")
+    self.language_magic(['a','b','c'])
+    pass
+
+
+
+###############################################
+
 def main():
   print("This is a class , don't run this directly")
   sampleOBJ=core("Kekre")
-  sampleOBJ.getStruct("sample")
-  sampleOBJ.readStruct()
-  sampleOBJ.tokenizeResponse("SampleString")
-  sampleOBJ.languageMagic(['a','b','c'])
+  sampleOBJ.run_core([1,2,3,4])
+  
 
 if __name__ == '__main__':
   main()
