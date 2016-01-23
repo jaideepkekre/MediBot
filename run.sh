dfdc38913eefdafa8c9167e221f6d7b8cbc00384
jaideepkekre <jaideepkekre@gmail.com>
2016-01-23 20:37:54 +0530

    new directory struct

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+#Owner :Jaideep Kekre
+#_author_ = Jaideep Kekre
+#_info_   = This module contains a Bash script 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+python $DIR/bin/server.py
