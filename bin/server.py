c9f575eb9a5d09db7764c54e59fb73502bd6d5be
Jaideep Kekre <jaideepkekre@gmail.com>
2016-01-25 13:17:40 +0530

    PEP8ifed server.py

--- bin/server.py
+++ bin/server.py
@@ -7,36 +7,39 @@ import os
 import threading
 from multiprocessing import Process, Queue
 from time import sleep
+
 from telegram import Updater
+
 import dispatcher
 
-TOKEN                  = os.environ.get('TELEGRAM_API_KEY')
-CREATOR                = dispatcher.dispatcher()
-MESSAGE_QUEUE          = Queue()
+TOKEN = os.environ.get('TELEGRAM_API_KEY')
+CREATOR = dispatcher.dispatcher()
+MESSAGE_QUEUE = Queue()
 FINISHED_MESSAGE_QUEUE = Queue()
 
+
 def return_messages(return_queue):
-  def actually_return_messages(resp):
-    m = resp[0]
-    bot = resp[1]
+    def actually_return_messages(resp):
+        m = resp[0]
+        bot = resp[1]
 
-    for text in m['response_list']:
-        bot.sendMessage(
-                chat_id=m['chat_id'], text=text, reply_markup=m['keyboard'])
+        for text in m['response_list']:
+            bot.sendMessage(
+                    chat_id=m['chat_id'], text=text, reply_markup=m['keyboard'])
 
-  user_thread_dict = {}
-  while True:
-    sleep(0.1)
-    if not return_queue.empty():
-      response = return_queue.get()
-      chat_id = response[0]['chat_id']
+    user_thread_dict = {}
+    while True:
+        sleep(0.1)
+        if not return_queue.empty():
+            response = return_queue.get()
+            chat_id = response[0]['chat_id']
 
-      if user_thread_dict.has_key(chat_id):
-          user_thread_dict[chat_id].join()
-          user_thread_dict.pop(chat_id)
+            if user_thread_dict.has_key(chat_id):
+                user_thread_dict[chat_id].join()
+                user_thread_dict.pop(chat_id)
 
-      user_thread_dict[chat_id] = threading.Thread(target=actually_return_messages, args=(response,))
-      user_thread_dict[chat_id].start()
+            user_thread_dict[chat_id] = threading.Thread(target=actually_return_messages, args=(response,))
+            user_thread_dict[chat_id].start()
 
 
 def dispatch_messages(queue_local):
@@ -48,6 +51,7 @@ def dispatch_messages(queue_local):
 
             FINISHED_MESSAGE_QUEUE.put((m, user_info['bot']))
 
+
 def accept_message(bot, update):
     d = {
         'chat_id': update.message.chat_id,
@@ -57,6 +61,7 @@ def accept_message(bot, update):
 
     MESSAGE_QUEUE.put(d)
 
+
 updater = Updater(token=TOKEN)
 message_sender = updater.dispatcher
 message_sender.addTelegramMessageHandler(accept_message)
@@ -65,6 +70,6 @@ message_processor = Process(target=dispatch_messages, args=(MESSAGE_QUEUE,))
 message_returner = Process(target=return_messages, args=(FINISHED_MESSAGE_QUEUE,))
 
 message_processor.start()
-message_returner .start()
+message_returner.start()
 updater.start_polling(poll_interval=0.1)
 updater.idle()
