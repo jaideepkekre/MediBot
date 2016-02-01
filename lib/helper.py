4de8000b4b86a8b6cccba1ee6640ba495349c42c
jaideepkekre <jaideepkekre@gmail.com>
2016-02-01 15:44:43 +0530

    PEP8

--- lib/helper.py
+++ lib/helper.py
@@ -15,11 +15,12 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 class question_interface_helper():
     """
     This class loads questions from data/ into legit question_interface objects.
     """
-  
+
     def __init__(self):
         pass
 
@@ -27,20 +28,21 @@ class question_interface_helper():
     @private
     assign attributes
     """
+
     @classmethod
     def _assign_attributes(self, q_interface, value, tag):
-        q_interface.tag             = value.get('tag') or tag
-        q_interface.question        = value['question']
-        q_interface.response        = value.get('response')
-        q_interface.response_type   = value['response_type']
-        q_interface.response_re     = value.get('response_re')
-        q_interface.ranged          = value.get('ranged')
-        q_interface.range           = value.get('range')
-        q_interface.ret             = value.get('ret')
-        q_interface.is_top_level    = value.get('is_top_level')
-        q_interface.loop            = value.get('loop')
+        q_interface.tag = value.get('tag') or tag
+        q_interface.question = value['question']
+        q_interface.response = value.get('response')
+        q_interface.response_type = value['response_type']
+        q_interface.response_re = value.get('response_re')
+        q_interface.ranged = value.get('ranged')
+        q_interface.range = value.get('range')
+        q_interface.ret = value.get('ret')
+        q_interface.is_top_level = value.get('is_top_level')
+        q_interface.loop = value.get('loop')
         q_interface.custom_keyboard = value.get('custom_keyboard')
-        q_interface.serial          = value.get('serial')
+        q_interface.serial = value.get('serial')
 
         return q_interface
 
@@ -48,7 +50,7 @@ class question_interface_helper():
     @classmethod
     def _build_linked_list(self, value, tag):
         from question_interface import question_interface
-        q    = question_interface()
+        q = question_interface()
         temp = q
 
         while True:
@@ -73,13 +75,12 @@ class question_interface_helper():
 
         for tag, value in data.iteritems():
             top = question_interface()
-            top_questions_list.append(self._assign_attributes(top,value, tag))
+            top_questions_list.append(self._assign_attributes(top, value, tag))
 
         return top_questions_list
 
     @classmethod
-    def load_linked_questions(self,symptom_tag,linked_question_tag=None,serial=None):
-        from question_interface import question_interface
+    def load_linked_questions(self, symptom_tag, linked_question_tag=None, serial=None):
 
         linked_questions_list = []
         data = __import__(symptom_tag).data()
@@ -89,17 +90,18 @@ class question_interface_helper():
                 linked_questions_list.append(self._build_linked_list(value, tag))
 
             linked_questions_list.sort(key=lambda x: x.serial)
-        elif serial == None and linked_question_tag: # search and load linked question by tag
+        elif serial == None and linked_question_tag:  # search and load linked question by tag
             for tag, value in data.iteritems():
                 if tag == linked_question_tag:
                     linked_questions_list.append(self._build_linked_list(value, tag))
 
-        elif linked_question_tag == None and serial: # search by serial
+        elif linked_question_tag == None and serial:  # search by serial
             for tag, value in data.iteritems():
                 if value['serial'] == serial:
                     linked_questions_list.append(self._build_linked_list(value, tag))
 
         return linked_questions_list
+
 
 def test():
     tq_list = question_interface_helper.load_top_questions()
@@ -109,7 +111,7 @@ def test():
         if q.question == "Do you have a fever?":
             f = q
 
-    if f.tag == 'fever' and f.response == ['Yes','No']:
+    if f.tag == 'fever' and f.response == ['Yes', 'No']:
         print "PASS"
 
     lq_list = question_interface_helper.load_linked_questions('fever')
@@ -122,18 +124,18 @@ def test():
     f = lq_list[0]
 
     if f.linked_questions.question == "Are any other joins hurting?" and f.tag == 'joint_pain_area' and f.linked_questions.tag == 'joint_pain_area_more_pain':
-       print "PASS"
+        print "PASS"
 
-    lq_list = question_interface_helper.load_linked_questions('fever',None,1)
+    lq_list = question_interface_helper.load_linked_questions('fever', None, 1)
 
     if len(lq_list) == 1:
         print "PASS"
     f = lq_list[0]
 
-    if f.tag == "fever_periodic" and f.question == "Are you having intermittent fever over time?" and f.response == ['Yes', 'No']:
+    if f.tag == "fever_periodic" and f.question == "Are you having intermittent fever over time?" and f.response == [
+        'Yes', 'No']:
         print "PASS"
 
 
 if __name__ == '__main__':
     test()
-
