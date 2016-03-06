8895109081a79e93b9ba01482415a82d366bd1d6
jaideepkekre <jaideepkekre@gmail.com>
2016-03-06 17:44:22 +0530

    clean up

--- expert_system/disease_interface.py
+++ expert_system/disease_interface.py
@@ -26,17 +26,39 @@ class Buckets:
     def __init__(self):
         self.bucket  = dict()
         self.symptom_score = dict()
+        self.diseases_object = Disease()
         self.populate_diseases()
 
-        
+    
     def add_symptom_score(self,symptom,score):
         if symptom in self.symptom_score.keys():
             self.symptom_score[symptom]=self.symptom_score[symptom]+score
         else :
             self.symptom_score[symptom]=score 
         
-        #Add new symptom here
-    
+    def remove_disease(self,name):
+        if name in self.bucket.keys():
+            diseases_dict=self.diseases_object.get_disease()
+            for specific_disease_name in diseases_dict.keys() :
+                if name == specific_disease_name:
+                    disease_dict=diseases_dict[specific_disease_name]
+                    for symptom in disease_dict.keys():
+                        self.remove_symptom_score(symptom, disease_dict)
+                    self.bucket.pop(disease_dict['name'])
+
+        pass
+    def remove_symptom_score(self,symptom,disease_dict):
+        if disease_dict[symptom] == None : 
+            print "symptom already asked , hence ignore"
+        elif symptom == 'name':
+            pass
+        else:
+            self.symptom_score[symptom]=self.symptom_score[symptom]-disease_dict[symptom]
+
+    def remove_symptom(self,symptom,disease_dict):
+        self.remove_symptom_score(symptom, score)
+        #table_disease_object.set_score(symptom,score)
+        #table_disease_object.set(symptom,arg)
 
     def set_table(self,symptom,score,table_disease_object,arg=True):
         self.add_symptom_score(symptom, score)
@@ -44,8 +66,8 @@ class Buckets:
         table_disease_object.set(symptom,arg)
     
     def populate_diseases(self):
-        diseases_object = Disease()
-        diseases_dict=diseases_object.get_disease()
+        
+        diseases_dict=self.diseases_object.get_disease()
         for specific_disease_name in diseases_dict.keys():
             new_table_obj         = symptom_validity_table()
             specific_disease_dict = diseases_dict[specific_disease_name]
@@ -70,4 +92,8 @@ if __name__ == '__main__':
     bucketlist = Buckets()
     print "Contents of Bucket are :" + str((bucketlist.bucket))
     print(bucketlist.symptom_score)
-    
+    bucketlist.remove_disease('dengue')
+    bucketlist.remove_disease('dengue')
+    bucketlist.remove_disease('dengue')
+    print "Contents of Bucket are :" + str((bucketlist.bucket))
+    print(bucketlist.symptom_score)
