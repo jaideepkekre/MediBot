a199b56f236358269788b946c2638cdad31edfea
jaideepkekre <jaideepkekre@gmail.com>
2016-03-07 14:51:32 +0530

    made disease.py a bit more generic

--- data/disease.py
+++ data/disease.py
@@ -3,42 +3,58 @@
 """
 author: Jaideep Kekre
 """
+'''
+Symptom Legend:
+'''
+fever = 'fever'
+body_pain = 'body_pain'
+joint_pain = 'joint_pain'
+pain_behind_eyes = 'pain_behind_eyes'
+rash='rash'
+clay_coloured_bowels = 'clay_coloured_bowels'
+fatigue = 'fatigue'
+yellow_eyes = 'yellow_eyes'
+
+'''
+Rating Legend:
+'''
+
 CRITICAL = 20
 IMPORTANT= 10
 OPTIONAL = 5
 
 dengue = {
-                'fever_measure' 	: CRITICAL,
-                'body_pain'			: IMPORTANT,
-                'joint_pain'		: IMPORTANT,
-                'pain_behind_eyes'	: OPTIONAL,
-                'rash'				: OPTIONAL,
-                'name'				:"dengue"
+                fever 	            : CRITICAL,
+                body_pain			: IMPORTANT,
+                joint_pain		    : IMPORTANT,
+                pain_behind_eyes	: OPTIONAL,
+                rash				: OPTIONAL,
+                'name'				: "dengue"
 
          }
 
 
 hepA=	{
-                'fever' 					: IMPORTANT,
-                'fatigue'		 			: CRITICAL,
-                'joint_pain'				: IMPORTANT,
-                'clay_coloured_bowels'		: IMPORTANT,
-                'yellow_eyes'				: OPTIONAL,
-                'name'						:"hepA"
+                fever 					: IMPORTANT,
+                fatigue		 			: CRITICAL,
+                joint_pain				: IMPORTANT,
+                clay_coloured_bowels	: IMPORTANT,
+                yellow_eyes				: OPTIONAL,
+                'name'					: "hepA"
          }
 
 
 diseases_dict = { 
-				'dengue'  :  dengue,
-				'hepA'    :  hepA,
+                dengue['name']  :  dengue,
+                hepA['name']    :  hepA,
 
-			}
+                }
 
 class Disease(object):
-	"""Loads data from Diease Dicts into a list of dicts"""
-	def __init__(self):
-		super(Disease, self).__init__()
-	def get_disease(self):
-		return diseases_dict
-		
+    """Loads data from Diease Dicts into a list of dicts"""
+    def __init__(self):
+        super(Disease, self).__init__()
+    def get_disease(self):
+        return diseases_dict
+
 
